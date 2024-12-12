222288c81a05a9f8ed1bf3ebd288ae9d88fbdecc
Raul Sigoli <raul_sigoli@hotmail.com>
2024-12-12 10:54:49 -0300

    feat: add header - responsivo v1

--- src/slices/Site/index.tsx
+++ src/slices/Site/index.tsx
@@ -1,5 +1,8 @@
+"use client"
+
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import { useState } from "react";
 import styles from "./Header.module.css";
 
 /**
@@ -11,17 +14,31 @@ export type SiteProps = SliceComponentProps<Content.SiteSlice>;
  * Component for "Site" Slices.
  */
 const Site = ({ slice }: SiteProps): JSX.Element => {
+  const [menuActive, setMenuActive] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuActive(!menuActive);
+  };
+
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>
-        <span className={styles.slash}>/</span>
-        <span className={styles.brand}>
-          <span className={styles.green}>Barão</span>
-          <span className={styles.blue}>Med</span>
-        </span>
-        <span className={styles.slash}>/</span>
+      <div className={styles["header-content"]}>
+        <div className={styles.logo}>
+          <span className={styles.slash}>/</span>
+          <span className={styles.brand}>
+            <span className={styles.green}>Barão</span>
+            <span className={styles.blue}>Med</span>
+          </span>
+          <span className={styles.slash}>/</span>
+        {/* Menu Toggle (hamburger) */}
+        <div className={styles["menu-toggle"]} onClick={toggleMenu}>
+          ☰
+        </div>
+        </div>
+
       </div>
-      <nav className={styles.nav}>
+
+      <nav className={`${styles.nav} ${menuActive ? styles.active : ""}`}>
         <a href="#" className={styles.link}>Home</a>
         <a href="#" className={styles.link}>Nossos serviços</a>
         <a href="#" className={styles.link}>Como funciona</a>
